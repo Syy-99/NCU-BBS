a2307286dab1c95d2d250a1f94eb1fe505089d2d
menxin <f58099516@163.com>
2019-12-17 21:38:25 +0800

    menxin

--- src/main/java/cn/edu/ncu/Entity/User.java
+++ src/main/java/cn/edu/ncu/Entity/User.java
@@ -3,4 +3,85 @@ package cn.edu.ncu.Entity;
 import java.io.Serializable;
 
 public class User implements Serializable {
+    private String uid;//账号
+    private String uname;//昵称
+    private String upassword;//密码
+    private String bonus;//积分
+    private String sex;//性别
+    private String email;//邮箱
+    private String address;//地址
+    private String age;//年龄
+    private String introduction;//个性签名
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public String getUpassword() {
+        return upassword;
+    }
+
+    public void setUpassword(String upassword) {
+        this.upassword = upassword;
+    }
+
+    public String getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(String bonus) {
+        this.bonus = bonus;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getIntroduction() {
+        return introduction;
+    }
+
+    public void setIntroduction(String introduction) {
+        this.introduction = introduction;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
 }
